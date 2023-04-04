5d6416d11a89688a9d19588cfa464d1531488471
sachinglarance <sachinglarance@gmail.com>
2023-04-04 22:50:57 +0530

    crud

--- dao/taskDetailsDAO.js
+++ dao/taskDetailsDAO.js
@@ -13,6 +13,55 @@ const TaskDetails = require('../module/task_details');
 //     }
 // };
 
+const createTask = async (data) => {
+    try {
+        let newOne =  await TaskDetails.create(data)
+        return JSON.parse(JSON.stringify(newOne));
+    } catch (err) {
+        console.log("Error..: ", err);
+    }
+}
+
+const updateTask = async (data) => {
+    try {
+        let updateOne =  await TaskDetails.update(data,{
+            where:{
+                task_id: id
+            }
+        })
+        return JSON.parse(JSON.stringify(updateOne));
+    } catch (err) {
+        console.log("Error..: ", err);
+    }
+}
+
+
+const deleteTask = async (data) => {
+    try {
+        let deleteOne =  await TaskDetails.destroy(data,{
+            where:{
+                task_id: id
+            }
+        })
+        return JSON.parse(JSON.stringify(deleteOne));
+    } catch (err) {
+        console.log("Error..: ", err);
+    }
+}
+
+const getTaskById = async (taskId) => {
+    try {
+        let taskbyId =  await TaskDetails.findOne({
+            where:{
+                task_id: taskId
+            }
+        })
+        return JSON.parse(JSON.stringify(taskbyId));
+    } catch (err) {
+        console.log("Error..: ", err);
+    }
+}
+
 const getAllTasks = async () => {
     try {
         const response = await TaskDetails.findAll();
@@ -25,8 +74,12 @@ const getAllTasks = async () => {
 
 
 module.exports = {
-    // createTask,
-    getAllTasks, 
-    
+    createTask : createTask,
+    updateTask : updateTask,
+    deleteTask : deleteTask,
+    getTaskById : getTaskById,
+    getAllTasks: getAllTasks,
+   
+
 };
 
--- server.js
+++ server.js
@@ -1,61 +1,94 @@
 const sequelize = require("./config/database");
 const taskDetailsDAO = require('./dao/taskDetailsDAO');
 const express = require('express');
-const task_details = require("./module/task_details");
 const app = express();
 const port = 3000;
+
+app.use(express.json()); // middleware to parse request body as json
 
 app.get('/', (req, res) => {
   res.send('This is sachin');
 });
 
-// app.get('/createTask', async (req, res) => {
-//   try {
-//     const newTask = {
-//       task_name : 'sachincharles'
-//     }
-//     const tasks = await taskDetailsDAO.createTask(newTask);
-//     res.send(tasks);
-//   } catch (err) {
-//     console.error("Error occurred while fetching tasks: ", err);
-//     res.status(500).send("An error occurred while fetching tasks. Please try again later.");
-//   }
-// });
+app.get('/createTask', async (req, res) => {
+    try {
+      let task = {
+        task_name: "sachin",
+      };
+      const newTask = await taskDetailsDAO.createTask(task);
+      res.send({
+        data : newTask,
+        message: 'Task created successfully',
+        status: 201
+      });
+    } catch (err) {
+      console.error("Error occurred while creating task: ", err);
+      res.status(500).send("An error occurred while creating task. Please try again later.");
+    }
+  });
+
+  app.get('/updateTask', async (req, res) => {
+    try {
+      let taskId = req.query.taskId;
+      let updateTask = {
+        task_name: "sachin",
+      };
+      const UpdateTask = await taskDetailsDAO.updateTask(taskId,updateTask);
+      res.send({
+        data : UpdateTask,
+        message: "Task updated succefully",
+        status: 201
+      });
+    } catch (err) {
+      console.error("Error occurred while creating task: ", err);
+      res.status(500).send("An error occurred while creating task. Please try again later.");
+    }
+  });
+
+
+  app.get('/deleteTask', async (req, res) => {
+    try {
+      let taskId = req.query.taskId;
+      const deleteTask = await taskDetailsDAO.deleteTask(taskId);
+      res.send({
+        data : deleteTask,
+        message: 'Task deleted successfully',
+        status: 201
+      });
+    } catch (err) {
+      console.error("Error occurred while creating task: ", err);
+      res.status(500).send("An error occurred while creating task. Please try again later.");
+    }
+  });
+
+  app.get('/getTaskbyId', async (req, res) => {
+    try {
+        let taskId = req.query.taskId;
+        const idTask = await taskDetailsDAO.deleteTask(taskId);
+        res.send({
+          data : idTask,
+          message: 'Task fetched successfully',
+          status: 201
+        });
+    } catch (err) {
+      console.error("Error occurred while fetching tasks: ", err);
+      res.status(500).send("An error occurred while fetching tasks. Please try again later.");
+    }
+  });
+
 
 app.get('/getAllTasks', async (req, res) => {
-  let response = {
-    code: 200,
-    message: "",
-    data: null,
-}
-try {
-    let tasks = await taskDetailsDAO.getAllTasks();
-    if (tasks) {
-        response = {
-            code: 200,
-            message: "Task fetched succefully",
-            data: tasks,
-        }
-    } else {
-        response = {
-            code: 500,
-            message: "Error occurred while fetching all tasks",
-            data: null,
-        }
-    }
-    res.send(response);
-} catch (err) {
-    console.log("Error occurred in all tasks fetching API: ", err);
-    response = {
-        code: 500,
-        message: "Error occurred while fetching all tasks",
-        data: null,
-    }
-    res.send(response);
-}
+  try {
+    const tasks = await taskDetailsDAO.getAllTasks();
+    res.send({
+      data: tasks,
+      message: 'Tasks retrieved successfully',
+      status: 200
+    });
+  } catch (err) {
+    console.error("Error occurred while fetching tasks: ", err);
+    res.status(500).send("An error occurred while fetching tasks. Please try again later.");
+  }
 });
-
-
-
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
